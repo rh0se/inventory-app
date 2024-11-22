0b553a424379f98fcbcacaec68d6d1c6e8396ba7
rh0se <momirhoda@gmail.com>
2024-11-22 12:24:09 +0100

    uodating routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"; 
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";  
 import MainLayout from './layouts/mainLayout';
 import AdminPage from "./pages/AdminPage";
 import UserPage from "./pages/UserPage";
@@ -6,8 +6,8 @@ import UserPage from "./pages/UserPage";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
-       <Route index element={<AdminPage />} />
-       <Route path="/user" element={<UserPage />} />
+       <Route index element={<UserPage />} /> {/* Default route: / */}
+       <Route path="admin" element={<AdminPage />} /> {/* Route: /admin */}
        <Route path="*" element={<h1>Page Not Found</h1>} /> {/* Fallback Route */}
     </Route>
   )
